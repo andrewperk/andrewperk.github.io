5af8fc67f7deba19b1e3508d58f600b0911f7c05
Andrew Perkins <andrewperk@gmail.com>
2024-04-14 09:36:18 -0500

    Finished hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,16 @@ function Hero() {
     <div name="hero" className="w-full h-screen bg-[#0a192f]">
         {/* Container */}
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-            <p>Hi, my name is</p>
-            <h1>Andrew Perkins</h1>
-            <p>I'm a front-end developer specializing in building awesome digital experiences. Currently, I'm focused on building responsive front-end web apps.</p>
+            <p className="text-red-600">Hi, my name is</p>
+            <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Andrew Perkins</h1>
+            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Front-end Developer</h2>
+            <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a front-end developer specializing in building awesome digital experiences. Currently, I'm focused on building responsive front-end web apps.</p>
             <div>
-                <button>View Work <HiArrowNarrowRight /></button>
+                <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600">View Work 
+                <span className="group-hover:rotate-90 duration-300">
+                    <HiArrowNarrowRight className="ml-3" />
+                </span>
+                </button>
             </div>
         </div>
     </div>
