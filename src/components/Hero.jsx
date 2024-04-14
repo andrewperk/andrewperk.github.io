6ed6b6bbab6cab7165968d6de822deb786e66a95
Andrew Perkins <andrewperk@gmail.com>
2024-04-14 18:58:33 -0500

    Changed some text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ function Hero() {
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
             <p className="text-red-600">Hi, my name is</p>
             <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Andrew Perkins</h1>
-            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Front-end Developer</h2>
-            <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a front-end developer specializing in building awesome digital experiences. Currently, I'm focused on building responsive front-end web apps.</p>
+            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full-Stack Developer</h2>
+            <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a full-stack developer specializing in building awesome digital experiences. Currently, I'm focused on building responsive full-stack web apps.</p>
             <div>
                 <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600">View Work 
                 <span className="group-hover:rotate-90 duration-300">
