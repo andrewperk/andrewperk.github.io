f8a3632ff19eb8d10c3486317e5a4eec41c25589
Andrew Perkins <andrewperk@gmail.com>
2024-04-14 19:36:09 -0500

    Finished react-scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,18 +18,50 @@ const Navbar = () => {
         {/* Menu */}
         <ul className="hidden md:flex">
             <li>
-            <Link 
-            to="hero" 
-            smooth={true} 
-            duration={500} 
-            >
-            Home
-            </Link>
+                <Link 
+                to="hero" 
+                smooth={true} 
+                duration={500} 
+                >
+                Home
+                </Link>
             </li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li>
+                <Link 
+                to="about" 
+                smooth={true} 
+                duration={500} 
+                >
+                About
+                </Link>
+            </li>
+            <li>
+                <Link 
+                to="skills" 
+                smooth={true} 
+                duration={500} 
+                >
+                Skills
+                </Link>
+            </li>
+            <li>
+                <Link 
+                to="work" 
+                smooth={true} 
+                duration={500} 
+                >
+                Work
+                </Link>
+            </li>
+            <li>
+                <Link 
+                to="contact" 
+                smooth={true} 
+                duration={500} 
+                >
+                Contact
+                </Link>
+            </li>
         </ul>
 
         {/* Hamburger */}
@@ -40,11 +72,56 @@ const Navbar = () => {
         {/* Mobile Menu */}
         <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
             <ul>
-                <li className="py-6 text-4xl">Home</li>
-                <li className="py-6 text-4xl">About</li>
-                <li className="py-6 text-4xl">Skills</li>
-                <li className="py-6 text-4xl">Work</li>
-                <li className="py-6 text-4xl">Contact</li>
+                <li className="py-6 text-4xl">
+                <Link 
+                onClick={handleClick} 
+                to="hero" 
+                smooth={true} 
+                duration={500} 
+                >
+                Home
+                </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                <Link
+                onClick={handleClick}  
+                to="about" 
+                smooth={true} 
+                duration={500} 
+                >
+                About
+                </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                <Link 
+                onClick={handleClick} 
+                to="skills" 
+                smooth={true} 
+                duration={500} 
+                >
+                Skills
+                </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                <Link 
+                onClick={handleClick} 
+                to="work" 
+                smooth={true} 
+                duration={500} 
+                >
+                Work
+                </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                <Link
+                onClick={handleClick}  
+                to="contact" 
+                smooth={true} 
+                duration={500} 
+                >
+                Contact
+                </Link>
+                </li>
             </ul>
         </div>
 
